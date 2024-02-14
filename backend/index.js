60289950b7f0d8e1d3d93f6e89795c273f91ed90
AnandAK07 <anandkumark055@gmail.com>
2024-02-14 18:45:35 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,13 @@ const connection=require('./config/db')
 const userRouter = require('./routes/user.route');
 const authentication = require('./middleware/authentication');
 const productRouter = require('./routes/product.route');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
 
 const app=express();
 app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('Apple is cutting')
@@ -15,10 +19,11 @@ app.use('/api', userRouter)
 app.use(authentication)
 app.use('/api',productRouter)
 
-app.listen(8080,async()=>{
+const PORT = process.env.PORT;
+app.listen(PORT, async () => {
     try {
         await connection()
-        console.log(`8080`)
+        console.log(`${PORT}`)
     } catch (error) {
         console.log('failed to conect')
     }
