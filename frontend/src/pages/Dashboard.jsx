fd7dcc28d40c3806e8ccc15c0cc229b95bb2da9a
AnandAK07 <anandkumark055@gmail.com>
2024-02-21 14:28:34 +0530

    figma css update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -12,6 +12,8 @@ import editImg from '../assets/edit-3.png'
 import moreImg from '../assets/more-horizontal.png'
 import trashImg from '../assets/trash-2.png'
 import sortImg from '../assets/Sort.png'
+import arrowImg from '../assets/arrow-right.png'
+import active from '../assets/Active.png'
 import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios'
 import { getAllProducts } from '../redux/productReducer/action';
@@ -136,17 +138,21 @@ export const Dashboard = () => {
         <div style={{
             display: 'flex', background: '#F8F8F8'
         }}>
-            <div style={{ background: '#FFFFFF', height: '1184px', width: '230px', border: '1px solid black' }}>
+
+            <div style={{ background: '#FFFFFF', height: '1184px', width: '230px' }}>
                 <h1 className='poppins-medium ' style={{ color: '#013CC6', fontSize: '24px', lineHeight: '36px', width: '203px', height: '36px', marginLeft: '18px', marginTop: '63px' }}>Nyka Dashboard</h1>
+
                 <div style={{ height: '993px', width: '131px', marginLeft: '41px', marginTop: '57px' }}>
-                    <div style={{ display: 'flex', flexWrap: 'wrap', alignContent: 'space-between', width: '131px', height: '154px' }}>
-                        <Link to={'/dashboard'}>
+
+                    <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', alignContent: 'space-between', width: '131px', height: '154px' }}>
+                        {/* <img src={active} alt="" /> */}
+                        <Link to={'/dashboard'} style={{ textDecoration: 'none', color: 'black' }}>
                             <div style={{ display: 'flex', justifyContent: 'space-between', width: '131px', height: '24px' }}>
                                 <img src={Dashboardimg} alt="" />
                                 <p style={{ width: '92px', height: '24px' }}>Dashboard</p>
                             </div>
                         </Link>
-                        <Link to={'/analytics'}>
+                        <Link to={'/analytics'} style={{ textDecoration: 'none', color: 'black' }}>
 
                             <div style={{ display: 'flex', justifyContent: 'space-between', width: '116px', height: '24px' }}>
                                 <img src={Analyticsimg} alt="" />
@@ -164,13 +170,13 @@ export const Dashboard = () => {
             </div>
 
             <div>
-                <div style={{ display: 'flex', justifyContent: 'space-between', width: '1129px', border: '1px solid pink', marginTop: '56.1px', marginLeft: '41px' }}>
-                    <div style={{ display: 'flex', background: '#FFFFFF', width: '655px', height: '52px', border: '1px solid green' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', width: '1129px', marginTop: '56.1px', marginLeft: '41px' }}>
+                    <div style={{ display: 'flex', background: '#FFFFFF', width: '655px', height: '52px', border: '1px solid #00000033' }}>
                         <img src={searchimg} alt="" style={{ width: '24px', height: '24px', marginTop: '13.9px', marginLeft: '20px' }} />
                         <input style={{ width: '57px', height: '24px', marginTop: '13.9px', marginLeft: '16px', border: 'none' }} placeholder='Search' />
                     </div>
 
-                    <div style={{ display: 'flex', gap: '20px', background: '#FFFFFF', width: '124px', height: '52px', border: '1px solid black' }}>
+                    <div style={{ display: 'flex', gap: '20px', background: '#FFFFFF', width: '124px', height: '52px' }}>
                         <div style={{ width: '52px', height: '52px' }}>
                             <img src={notification} alt="" style={{ width: '24px', height: '24px', marginTop: '14px', marginLeft: '14px' }} />
                         </div>
@@ -182,21 +188,21 @@ export const Dashboard = () => {
 
                 <div style={{ display: 'flex', marginTop: '33.9px' }}>
                     <div>
-                        <select value={filterGender} onChange={e => setFilterGender(e.target.value)} name="Filter By Gender" id="gender-filter" style={{ background: '#FFFFFF', border: '1px solid black', width: '229.2px', height: '51.8px', marginLeft: '61px' }}>
+                        <select value={filterGender} onChange={e => setFilterGender(e.target.value)} name="Filter By Gender" id="gender-filter" style={{ background: '#FFFFFF', border: '1px solid #7949FF', borderRadius: '8px', width: '229.2px', height: '51.8px', marginLeft: '61px' }}>
                             <option value="placeholder" style={{ display: "none" }}>Filter By Gender</option>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>
+                            <option value="male" >Male</option>
+                            <option value="female" >Female</option>
                         </select>
-                        <select value={filterCategory} onChange={e => setFilterCategory(e.target.value)} name="Filter By Category" id="gender-filter" style={{ background: '#FFFFFF', border: '1px solid black', width: '229.2px', height: '43px', marginLeft: '60.8px' }}>
+                        <select value={filterCategory} onChange={e => setFilterCategory(e.target.value)} name="Filter By Category" id="gender-filter" style={{ background: '#FFFFFF', border: '1px solid #7949FF', borderRadius: '8px', width: '229.2px', height: '43px', marginLeft: '60.8px' }}>
                             <option value="placeholder" style={{ display: "none" }}>Filter By Category</option>
                             <option value="makeup">Makeup</option>
                             <option value="skincare">Skincare</option>
                             <option value="haircare">Haircare</option>
                         </select>
 
-                        <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)} name="Sort By Price" id="gender-filter" style={{ background: '#FFFFFF', border: '1px solid black', width: '229.2px', height: '56.4px', marginLeft: '60.8px' }}>
+                        <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)} name="Sort By Price" id="gender-filter" style={{ background: '#FFFFFF', border: '1px solid black', border: '1px solid #7949FF', borderRadius: '8px', width: '229.2px', height: '56.4px', marginLeft: '60.8px' }}>
                             <option value="placeholder" style={{ display: "none", height: '52px' }}>Sort By Price</option>
-                            <option value="asc" style={{ height: '520px', padding: '264px', background: 'red', marginTop: '200px' }}>Ascending</option>
+                            <option value="asc" >Ascending</option>
                             <option value="desc" style={{ height: '52px' }}>Descending</option>
                         </select>
                     </div>
@@ -205,13 +211,16 @@ export const Dashboard = () => {
                     ADD PRODUCT
                 </button>
 
-                <div style={{ background: '#FFFFFF', width: '1216px', height: '830px', border: '1px solid blue', marginTop: '46px', marginLeft: '33px' }}>
+                <div style={{ background: '#FFFFFF', width: '1216px', height: '830px', border: '1px solid #3326AE14', marginTop: '46px', marginLeft: '33px', borderRadius: '16px' }}>
                     <div style={{ display: 'flex', marginTop: '32px', marginLeft: '32px' }}>
-                        <p className='inter-500' style={{ width: '131px', height: '30px'}}>Latest Orders</p>
-                        <p style={{ marginLeft: '958px', marginTop: '4px' }}>More</p>
+                        <p className='inter-500' style={{ width: '131px', height: '30px' }}>Latest Orders</p>
+                        <div style={{ display: 'flex', marginLeft: '958px', marginTop: '4px' }}>
+                            <p style={{ width: '35px', height: '22px' }}>More</p>
+                            <img src={arrowImg} alt="" style={{ display: 'flex', alignItems: 'center', width: '16px', height: '16px', marginTop: '2px', marginLeft: '12px' }} />
+                        </div>
                     </div>
 
-                    <div style={{ display: 'flex', width: '1152px', height: '64px', border: '1px solid green', marginTop: '32px', marginLeft: '32px' }}>
+                    <div style={{ display: 'flex', width: '1152px', height: '64px', marginTop: '32px', marginLeft: '32px', background: '#F8F8F8' }}>
 
                         <div style={{ width: '62px', height: "22px", marginTop: '21px', marginLeft: '40px' }}>
                             <p className='inter-600 ' style={{ width: '62px', height: "22px" }}>Products</p>
@@ -245,17 +254,17 @@ export const Dashboard = () => {
                         </div>
                     </div>
                     {currentProducts?.map((item, index) => {
-                        return <div key={index} style={{ display: 'flex', width: '1152px', height: '64px', border: '1px solid green', marginLeft: '32px' }}>
+                        return <div key={index} style={{ display: 'flex', width: '1152px', height: '64px', marginLeft: '32px', borderTop: '1px solid #00000033' }}>
                             <img src={item.picture} alt="" style={{ width: '44px', height: '44px', marginTop: '10px', marginLeft: '32px' }} />
-                            <p className='inter-400' style={{ color: '#555F7E', width: '63px', height: '22px', marginTop: '21px', marginLeft: '16px' }}>{`Product ${index + 1}`}</p>
+                            <p className='inter-400' style={{ color: '#555F7E', width: '65px', height: '22px', marginTop: '21px', marginLeft: '16px' }}>{`Product ${index + 1}`}</p>
                             {/* <p style={{ width: '63px', height: '22px', marginTop: '21px', marginLeft: '16px' }}>{item.name}</p> */}
                             <p style={{ color: '#555F7E', width: '48px', height: '22px', marginTop: '21px', marginLeft: '183px' }}>{item.gender}</p>
                             <p style={{ color: '#555F7E', width: '57px', height: '22px', marginTop: '21px', marginLeft: '85px' }}>{item.category}</p>
                             <p style={{ color: '#555F7E', width: '56px', height: '22px', marginTop: '21px', marginLeft: '106px' }}>${item.price}</p>
                             {/* <div style={{ display: 'flex', flexDirection: 'row', width: '366px' }}>
                             </div> */}
-                            <p style={{ border: '1px solid black', color: '#555F7E', width: '366px', height: '61px', marginLeft: '77px', marginTop: '21px' }}>{item.description}</p>
-                            <div style={{ width: '80px', height: '16px', border: '1px solid black', marginLeft: '-96px', marginTop: '24px', gap: '16px' }}>
+                            <p style={{ color: '#555F7E', width: '366px', height: '61px', marginLeft: '77px', marginTop: '8px' }}>{item.description}</p>
+                            <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center", width: '80px', height: '16px', marginLeft: '-96px', marginTop: '24px', gap: '16px' }}>
                                 <img src={editImg} alt="" style={{ width: '16px' }} onClick={() => handleEdit(item._id)} />
                                 <img src={trashImg} alt="" style={{ width: '16px' }} onClick={() => handleDelete(item._id)} />
                                 <img src={moreImg} alt="" style={{ width: '16px' }} onClick={() => handleDetails(item._id)} />
